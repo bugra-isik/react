e14efd91ad68ce414a810d2222540c4994887e63
Buğra <librouse98@gmail.com>
2023-03-12 04:01:32 +0300

    Version 1.0.0

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
+import { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    currentCategory: "",
+    products: [],
+  };
+  changeCategory = (change) => {
+    this.setState({
+      currentCategory: change.categoryName,
+    });
+    this.getProducts(change.id);
+  };
+  componentDidMount() {
+    this.getProducts();
+  }
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          products: data,
+        })
+      );
+  };
+  render() {
+    let categoryInfo = {
+      title: "zort List",
+    };
+
+    let productInfo = {
+      title: "Product List",
+    };
+
+    return (
+      <div>
+        <Container>
+          <Navi />
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                currentProduct={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
---
+++ src/ProductList.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Table, Button } from "reactstrap";
+
+export default class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  addToCart = (a) => {
+    alert(a.productName);
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <h3>{this.props.info.title}</h3> */}
+        <h3>{this.props.currentCategory}</h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Quantity Per Unit</th>
+              <th>Unit Price</th>
+              <th>Units In Stock</th>
+              <th></th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            {this.props.currentProduct.map((product) => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+
+                <td>{product.productName}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitPrice}</td>
+                <td>{product.unitsInStock}</td>
+                <td>
+                  <Button color="info" onClick={() => this.addToCart(product)}>
+                    Add{}
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
